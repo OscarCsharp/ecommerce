ef9bfe0e28bf5161ff1ef996a485f859df00cfaa
oscarndlovu <102793760+oscarndlovu@users.noreply.github.com>
2023-02-21 15:32:31 +0200

    cors test

--- application/index.php
+++ application/index.php
@@ -1,5 +1,7 @@
+
 <?php
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin");
    ob_start();
    session_start();
 ?>
